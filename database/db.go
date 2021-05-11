a9004665abb5054107f4d5208d9ff3c7d88da736
Nikolay Nikolaev <66773789+Notarrogantjustbetter@users.noreply.github.com>
2021-05-11 18:52:17 +0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -1,1 +1,34 @@
+package database
+
+import (
+	"github.com/go-redis/redis"
+)
+
+var client *redis.Client
+
+
+func InitRedis() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+}
+
+type RedisService interface {
+	CreateTask(task string) error
+	DeleteTask(task int) error
+	GetTasks()([]string, error)
+}
+
+func CreateTask(task string) error {
+	return client.LPush("Tasks", task).Err()
+}
+
+func DeleteTask(task string) error {
+	return client.LRem("Tasks", 1, task).Err()
+}
+
+func GetTasks()([]string, error) {
+	return client.LRange("Tasks", 0, 100).Result()
+}
+
 
